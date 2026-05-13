f231e0dc81dccf90d003af840289307d32ec117a
mauriciojoseoro-afk <mauriciojoseoro@gmail.com>
2026-05-13 16:22:38 +0000

    City, Node y files

--- files.py
+++ files.py
@@ -1,1 +1,19 @@
+def read_divipola(self, file_path):
+    countries = DoubleLinkedList()
+    colombia = Country("CO", "Colombia")
+    countries.append(colombia)
 
+    departments = {}
+
+    with open(file_path, encoding="utf-8-sig") as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            dept_code = row["cod_depto"]
+            dept_name = row["departamento"]
+
+            city_code = row["cod_mpio"]
+            city_name = row["municipio"]
+
+            lat = row["lat"]
+            lon = row["lon"]
